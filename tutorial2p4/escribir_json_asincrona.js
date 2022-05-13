c48b2226bb5a5c0a3d556b32e6c012ccfd0449a8
Julio Daniel Guerra Hollstein <mallium@gmail.com>
2022-05-13 09:01:15 -0400

    nuevos cambios

--- tutorial2p4/escribir_json_asincrona.js
+++ tutorial2p4/escribir_json_asincrona.js
@@ -1,24 +1,22 @@
-const fs = require('fs');
-
+const fs = require("fs");
+const a = 0;
 let user = {
-    name: 'John Doe',
-    emai: 'john.doe@example.com',
-    age: 27,
-    gender: 'Male',
-    profession: 'Software Developer'
+  name: "John Doe",
+  emai: "john.doe@example.com",
+  age: 27,
+  gender: "Male",
+  profession: "Software Developer",
 };
 
 // pretty-print JSON object to string
 const data = JSON.stringify(user, null, 4);
 // write file to disk
-fs.writeFile('./user.json', data, 'utf8', (err) => {
-
-    if (err) {
-        console.log(`Error writing file: ${err}`);
-    } else {
-        console.log(`File is written successfully!`);
-    }
-
+fs.writeFile("./user.json", data, "utf8", (err) => {
+  if (err) {
+    console.log(`Error writing file: ${err}`);
+  } else {
+    console.log(`File is written successfully!`);
+  }
 });
 
 console.log("Terminado!");
